1f290cbbd740d88bd2c6baccdb0c5c1a55711fe3
undranana <ene_undraa@yahoo.com>
2017-07-01 18:51:56 +0900

    [IMP]allowed cash and warehouses on user. barcode on product. tree view of product.

--- gp_shoes_warehouse/model/product.py
+++ gp_shoes_warehouse/model/product.py
@@ -14,7 +14,6 @@ class ProductProduct(models.Model):
     _description = "Product"
     _inherit = ['product.product']
     _order = 'default_code, id'
-
     @api.multi
     def name_get(self):
         # TDE: this could be cleaned a bit I think
@@ -119,3 +118,14 @@ class ProductProduct(models.Model):
             products = self.search(args, limit=limit)
         return products.name_get()
 
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+    type = fields.Selection([
+        ('consu', _('Consumable')),
+        ('service', _('Service')),
+        ('product', _('Stockable Product'))], string='Product Type', default='product', required=True,
+        help='A stockable product is a product for which you manage stock. The "Inventory" app has to be installed.\n'
+             'A consumable product, on the other hand, is a product for which stock is not managed.\n'
+             'A service is a non-material product you provide.\n'
+             'A digital content is a non-material product you sell online. The files attached to the products are the one that are sold on '
+             'the e-commerce such as e-books, music, pictures,... The "Digital Product" module has to be installed.')
---
+++ gp_shoes_warehouse/model/res_users.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import re
+
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError
+from odoo.osv import expression
+
+import odoo.addons.decimal_precision as dp
+
+class Users(models.Model):
+    _inherit = ['res.users']
+    allowed_cash = fields.Many2many('cash', string = 'Allowed Cash')
+    allowed_warehouses = fields.Many2many('stock.warehouse', string='allowed Warehouse')
