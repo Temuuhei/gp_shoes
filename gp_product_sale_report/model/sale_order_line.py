ab29e5eb871020ed5fb0cc6ecb8245ba463a347a
Temuuhei <temuujintsogt@gmail.com>
2018-01-07 10:20:46 +0800

    onchange card cash payment

--- gp_product_sale_report/model/sale_order_line.py
+++ gp_product_sale_report/model/sale_order_line.py
@@ -7,3 +7,19 @@ class SaleOrderLine(models.Model):
 
     cash_payment = fields.Float('Cash payment', default=0)
     card_payment = fields.Float('Card payment', default=0)
+
+    @api.onchange('cash_payment')
+    def onChangeCash(self):
+        for object in self:
+            limit = object.product_uom_qty * object.price_unit
+            if limit < object.cash_payment:
+                object.cash_payment = limit
+            object.card_payment = limit - object.cash_payment
+
+    @api.onchange('card_payment')
+    def onChangeCard(self):
+        for object in self:
+            limit = object.product_uom_qty * object.price_unit
+            if limit < object.card_payment:
+                object.card_payment = limit
+            object.cash_payment = limit - object.card_payment
