337f0069c8bbafe55dd73a252420ddb219cd3050
Sarul <Sarul>
2018-02-26 15:25:33 +0800

    fix

--- gp_shoes_warehouse/model/product.py
+++ gp_shoes_warehouse/model/product.py
@@ -155,7 +155,6 @@ class ProductTemplate(models.Model):
              'A service is a non-material product you provide.\n'
              'A digital content is a non-material product you sell online. The files attached to the products are the one that are sold on '
              'the e-commerce such as e-books, music, pictures,... The "Digital Product" module has to be installed.')
-    barcode = fields.Char('Barcode', store=True)
     barcode = fields.Char('Barcode', related='product_variant_ids.new_barcode', store=True)
     main_price = fields.Float('Main price', default=0)
 
