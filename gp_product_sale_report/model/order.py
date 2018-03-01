c202abc8ab5ee407226a7a8f055494d62f80278a
Sarul <Sarul>
2018-03-01 18:26:00 +0800

    erase default code order

--- gp_product_sale_report/model/order.py
+++ gp_product_sale_report/model/order.py
@@ -4,18 +4,18 @@ from odoo import api, fields, models, tools, _
 
 class StockQuant(models.Model):
     _inherit = "stock.quant"
-    _order = "default_code_integer asc"
+    # _order = "default_code_integer asc"
 
     # default_code_integer = fields.Integer(related='product_id.product_tmpl_id.default_code_integer', string="Default code")
 
 class StockMove(models.Model):
     _inherit = "stock.move"
-    _order = "default_code_integer asc"
+    # _order = "default_code_integer asc"
 
     # default_code_integer = fields.Integer(related='product_id.product_tmpl_id.default_code_integer', string="Default code")
 
 class StockInventoryLine(models.Model):
     _inherit = "stock.inventory.line"
-    _order = "default_code_integer asc"
+    # _order = "default_code_integer asc"
 
     # default_code_integer = fields.Integer(related='product_id.product_tmpl_id.default_code_integer', string="Default code")
