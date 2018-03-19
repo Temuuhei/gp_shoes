949243c1dfd729f0d2deea98588aaff701a100d3
Sarul <Sarul>
2018-03-19 13:12:21 +0800

    do_new_transfer// process constraint not 0 qty

--- gp_product_sale_report/model/stock_picking.py
+++ gp_product_sale_report/model/stock_picking.py
@@ -60,7 +60,7 @@ class StockImmediateTransfer(models.TransientModel):
         prcs = super(StockImmediateTransfer, self).process()
         sp = self.env['stock.picking'].browse(self.env.context.copy()['active_id'])
         for ml in sp.move_lines:
-            quant = self.env['stock.quant'].search([('location_id', '=', self.location_id.id),
+            quant = self.env['stock.quant'].search([('location_id', '=', sp.location_id.id),
                                                     ('product_id', '=', ml.product_id.id)])
             if quant.qty < ml.product_uom_qty or not quant.qty:
                 raise ValidationError(_('There is no product in your stock or not enough!'), ml.product_id.product_tmpl_id.name)
