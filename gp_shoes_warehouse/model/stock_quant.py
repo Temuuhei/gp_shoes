67ab6856966c342db36c49922e13eb4344f785f7
Temuuhei <temuujin.ts@mak.mn>
2019-06-06 20:21:22 +0800

    [ADD] Агуулахын хөдөлгөөний товлогдсон огноог байнга авч явах

--- gp_shoes_warehouse/model/stock_quant.py
+++ gp_shoes_warehouse/model/stock_quant.py
@@ -135,6 +135,105 @@ class Picking(models.Model):
         default= _default_stock_picking)
     inv = fields.Boolean('Create user',compute='_compute_your_picking')
 
+    @api.multi
+    def do_transfer(self):
+        """ If no pack operation, we do simple action_done of the picking.
+        Otherwise, do the pack operations. """
+        # TDE CLEAN ME: reclean me, please
+        self._create_lots_for_picking()
+
+        no_pack_op_pickings = self.filtered(lambda picking: not picking.pack_operation_ids)
+        no_pack_op_pickings.action_done()
+        other_pickings = self - no_pack_op_pickings
+        for picking in other_pickings:
+            need_rereserve, all_op_processed = picking.picking_recompute_remaining_quantities()
+            todo_moves = self.env['stock.move']
+            toassign_moves = self.env['stock.move']
+
+            # create extra moves in the picking (unexpected product moves coming from pack operations)
+            if not all_op_processed:
+                todo_moves |= picking._create_extra_moves()
+
+            if need_rereserve or not all_op_processed:
+                moves_reassign = any(x.origin_returned_move_id or x.move_orig_ids for x in picking.move_lines if
+                                     x.state not in ['done', 'cancel'])
+                if moves_reassign and picking.location_id.usage not in ("supplier", "production", "inventory"):
+                    # unnecessary to assign other quants than those involved with pack operations as they will be unreserved anyways.
+                    picking.with_context(reserve_only_ops=True, no_state_change=True).rereserve_quants(
+                        move_ids=picking.move_lines.ids)
+                picking.do_recompute_remaining_quantities()
+
+            # split move lines if needed
+            for move in picking.move_lines:
+                rounding = move.product_id.uom_id.rounding
+                remaining_qty = move.remaining_qty
+                if move.state in ('done', 'cancel'):
+                    # ignore stock moves cancelled or already done
+                    continue
+                elif move.state == 'draft':
+                    toassign_moves |= move
+                if float_compare(remaining_qty, 0, precision_rounding=rounding) == 0:
+                    if move.state in ('draft', 'assigned', 'confirmed'):
+                        todo_moves |= move
+                elif float_compare(remaining_qty, 0, precision_rounding=rounding) > 0 and float_compare(remaining_qty,
+                                                                                                        move.product_qty,
+                                                                                                        precision_rounding=rounding) < 0:
+                    # TDE FIXME: shoudl probably return a move - check for no track key, by the way
+                    new_move_id = move.split(remaining_qty)
+                    new_move = self.env['stock.move'].with_context(mail_notrack=True).browse(new_move_id)
+                    todo_moves |= move
+                    # Assign move as it was assigned before
+                    toassign_moves |= new_move
+                #edit here by Tr1um
+                move.write({'date':picking.min_date})
+
+            # TDE FIXME: do_only_split does not seem used anymore
+            if todo_moves and not self.env.context.get('do_only_split'):
+                todo_moves.action_done()
+            elif self.env.context.get('do_only_split'):
+                picking = picking.with_context(split=todo_moves.ids)
+
+            picking._create_backorder()
+        return True
+
+
+class StockImmediateTransfer(models.TransientModel):
+    _inherit = 'stock.immediate.transfer'
+    _description = 'Immediate Transfer'
+
+
+    @api.model
+    def default_get(self, fields):
+        res = super(StockImmediateTransfer, self).default_get(fields)
+        if not res.get('pick_id') and self._context.get('active_id'):
+            res['pick_id'] = self._context['active_id']
+        return res
+
+    @api.multi
+    def process(self):
+        self.ensure_one()
+        # If still in draft => confirm and assign
+        if self.pick_id.state == 'draft':
+            self.pick_id.action_confirm()
+            if self.pick_id.state != 'assigned':
+                self.pick_id.action_assign()
+                if self.pick_id.state != 'assigned':
+                    raise UserError(_("Could not reserve all requested products. Please use the \'Mark as Todo\' button to handle the reservation manually."))
+        for pack in self.pick_id.pack_operation_ids:
+            if pack.product_qty > 0:
+                pack.write({'qty_done': pack.product_qty})
+            else:
+                pack.unlink()
+        self.pick_id.write({'date_done':self.pick_id.min_date})
+        stock_move = self.env['stock.move'].search(
+            [('picking_id', '=', self.pick_id.id)])
+        if stock_move:
+            for sm in stock_move:
+                sm.write({'date': self.pick_id.min_date})
+                print 'sm date', sm.date
+
+        return self.pick_id.do_transfer()
+
 class Inventory(models.Model):
     _name = "stock.inventory"
     _description = "Inventory"
